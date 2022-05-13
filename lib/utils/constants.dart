685885b7b24c543e48f359b6aa7ffb93f888c1ba
Ankit Kumar <Ankit.Kumar@guitarcenter.com>
2022-05-13 13:58:53 -0700

    constants.dart edited online with Bitbucket

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,8 +4,8 @@ const Map<String, dynamic> authJson = {
       '3MVG9KI2HHAq33RxCZsmOszHULYFbzfZ5N3n.ZbXHHObojG0a1tIEz7c5k749ZRW5n2xGcGxqy2OYw8zI0VGj',
   'client_secret':
       'E062FCA77D3FE219F8E4AE7A09B74ADEDC45032D2107FC1360DB24EFB693E502',
-  'username': 'eaisupport@guitarcenter.com',
-  'password': 'T!bC0\$6\$#3!R0\$j6xZ5WbXCZYNJOCFpRfesG3S',
+  'username': 'customerconnect@guitarcenter.com',
+  'password': 'm0bileus3r123',
 };
 
 Map<String, String> authHeaders = {
