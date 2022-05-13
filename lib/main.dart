17e507b27a3e8fa6b53f049e47b44adaad0f5d49
Rajas Chougule <norequirement@gmail.com>
2022-05-13 19:59:14 +0530

    This commit includes -> Bugfix

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'package:salesforce_spo/presentation/tabs/home_tab.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await _acquireToken();
+  await _acquireToken();
   runApp(const MyApp());
 }
 
