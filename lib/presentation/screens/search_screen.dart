9ad9c726febce18b47a361a667ad3a72f0522574
Rajas Chougule <norequirement@gmail.com>
2022-05-16 18:38:30 +0530

    This commit includes -> Minor bug fixes

--- lib/presentation/screens/search_screen.dart
+++ lib/presentation/screens/search_screen.dart
@@ -77,26 +77,36 @@ class _SearchScreenState extends State<SearchScreen> {
             child: FutureBuilder(
               future: futureCustomers,
               builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-                return ListView.builder(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  shrinkWrap: true,
-                  itemCount: customers.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return CustomerDetailsCard(
-                      customerId: customers[index].id,
-                      firstName: customers[index].firstName,
-                      lastName: customers[index].lastName,
-                      email: customers[index].email,
-                      phone: customers[index].phone,
-                      preferredInstrument:
-                          customers[index].preferredInstrument,
-                      lastTransactionDate: customers[index].lastTransactionDate,
-                      ltv: customers[index].lifetimeNetUnits,
-                      averageProductValue: customers[index].lifeTimeNetSalesAmount,
-                      customerLevel: customers[index].medianLTVNet,
+                switch(snapshot.connectionState){
+                  case ConnectionState.none:
+                    return const SizedBox.shrink();
+                  case ConnectionState.waiting:
+                  case ConnectionState.active:
+                    return const Center(
+                      child: CircularProgressIndicator(),
                     );
-                  },
-                );
+                  case ConnectionState.done:
+                    return ListView.builder(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      shrinkWrap: true,
+                      itemCount: customers.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return CustomerDetailsCard(
+                          customerId: customers[index].id,
+                          firstName: customers[index].firstName,
+                          lastName: customers[index].lastName,
+                          email: customers[index].email,
+                          phone: customers[index].phone,
+                          preferredInstrument:
+                          customers[index].preferredInstrument,
+                          lastTransactionDate: customers[index].lastTransactionDate,
+                          ltv: customers[index].lifetimeNetUnits,
+                          averageProductValue: customers[index].lifeTimeNetSalesAmount,
+                          customerLevel: customers[index].medianLTVNet,
+                        );
+                      },
+                    );
+                }
               },
             ),
           ),
