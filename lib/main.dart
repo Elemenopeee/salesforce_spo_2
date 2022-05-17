fcdbff4ec8467e97893d1677b0db908bac258f15
Ankit Kumar <Ankit.Kumar@guitarcenter.com>
2022-05-16 17:32:38 -0700

    App name

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Customer Connect',
+      title: 'GC Customer Connect Sandbox',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
