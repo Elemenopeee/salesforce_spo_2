56403454f5270b4ffab63068ada13f2b9d4e9d8d
shrutika.tatkare <shrutika.tatkare@apptware.com>
2022-05-24 10:39:15 +0530

    UI changes in tabs

--- lib/presentation/screens/tab_screens/case_screen.dart
+++ lib/presentation/screens/tab_screens/case_screen.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:salesforce_spo/common_widgets/no_data_found.dart';
-import 'package:salesforce_spo/design_system/primitives/icon_system.dart';
 import 'package:salesforce_spo/design_system/primitives/padding_system.dart';
 import 'package:salesforce_spo/design_system/primitives/size_system.dart';
 import 'package:salesforce_spo/utils/constants.dart';
 
 import '../../../design_system/primitives/color_system.dart';
-import '../../../models/cases.dart';
-import '../../../services/networking/endpoints.dart';
-import '../../../services/networking/networking_service.dart';
+import '../../../design_system/primitives/landing_images.dart';
+import '../../../design_system/primitives/music_icons_system.dart';
 
 class CasesProductList extends StatefulWidget {
   const CasesProductList({Key? key}) : super(key: key);
@@ -19,62 +16,57 @@ class CasesProductList extends StatefulWidget {
 }
 
 class _CasesProductListState extends State<CasesProductList> {
-  List<Cases> casesList = [];
-  bool isLoading = true;
-  @override
-  void initState() {
-    getCasesList();
-    super.initState();
-  }
+  var listOfCasesImage = [
+    LandingImages.guitarNew,
+    LandingImages.guitarNew,
+    LandingImages.guitarNew,
+    MusicIconsSystem.bassGuitar,
+    MusicIconsSystem.bassGuitar,
+    LandingImages.guitar,
+  ];
 
-  Future<void> getCasesList() async {
-    var response =
-        await HttpService().doGet(path: Endpoints.getClientCases(''));
-    if (response.data != null && response.data['records'].length > 0) {
-      response.data['records'].forEach((record) {
-        casesList.add(Cases.fromJson(record));
-      });
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
+  var listOfProductName = [
+    "Product Name",
+    "Product Name",
+    "Guitar Name",
+    "Guitar Name",
+    "Product Name",
+    "Product Name",
+  ];
 
+  var listOfProductStatus = [
+    "Broken Product",
+    "Returned",
+    "Refund initiated",
+    "Wrong Product",
+    "Returned",
+    "Refund initiated",
+  ];
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? Column(children: const [
-            SizedBox(
-              height: PaddingSystem.padding20,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: PaddingSystem.padding10),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.83,
+        width: double.infinity,
+        child: GridView.builder(
+            itemCount: listOfCasesImage.length,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 4 / 5,
+              crossAxisSpacing: SizeSystem.size8,
+              mainAxisSpacing: SizeSystem.size0,
             ),
-            CircularProgressIndicator()
-          ])
-        : casesList.isEmpty
-            ? const Center(
-                child: NoDataFound(),
-              )
-            : Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: PaddingSystem.padding10,
-                    vertical: PaddingSystem.padding20),
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.83,
-                  width: double.infinity,
-                  child: GridView.builder(
-                      itemCount: casesList.length,
-                      physics: const NeverScrollableScrollPhysics(),
-                      gridDelegate:
-                          const SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 200,
-                        childAspectRatio: 0.7,
-                        crossAxisSpacing: SizeSystem.size8,
-                        mainAxisSpacing: SizeSystem.size0,
-                      ),
-                      itemBuilder: (context, index) {
-                        return getSingleCasesList(context, index);
-                      }),
-                ),
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  getSingleCasesList(context, index),
+                ],
               );
+            }),
+      ),
+    );
   }
 
   Widget getSingleCasesList(BuildContext context, int index) {
@@ -82,7 +74,7 @@ class _CasesProductListState extends State<CasesProductList> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
-          height: MediaQuery.of(context).size.height * 0.20,
+          height: MediaQuery.of(context).size.height * 0.19,
           width: MediaQuery.of(context).size.height * 0.20,
           decoration: BoxDecoration(
             border: Border.all(
@@ -93,7 +85,7 @@ class _CasesProductListState extends State<CasesProductList> {
           ),
           child: Padding(
             padding: const EdgeInsets.only(bottom: SizeSystem.size5),
-            child: SvgPicture.asset(casesList[index].attributes.url,
+            child: SvgPicture.asset(listOfCasesImage[index],
                 color: Colors.black87),
           ),
         ),
@@ -101,7 +93,7 @@ class _CasesProductListState extends State<CasesProductList> {
           height: SizeSystem.size10,
         ),
         Text(
-          casesList[index].reason.toString(),
+          listOfProductName[index],
           style:
               const TextStyle(fontSize: SizeSystem.size16, fontFamily: kRubik),
         ),
@@ -109,7 +101,7 @@ class _CasesProductListState extends State<CasesProductList> {
           height: SizeSystem.size10,
         ),
         Text(
-          casesList[index].status,
+          listOfProductStatus[index],
           style:
               const TextStyle(fontSize: SizeSystem.size12, fontFamily: kRubik),
         ),
--- lib/presentation/screens/tab_screens/promos_screen.dart
+++ lib/presentation/screens/tab_screens/promos_screen.dart
@@ -1,62 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:salesforce_spo/common_widgets/promo.dart';
-import 'package:salesforce_spo/design_system/primitives/padding_system.dart';
-import '../../../common_widgets/no_data_found.dart';
+
+import '../../../common_widgets/note.dart';
 import '../../../models/promo_model.dart';
-import '../../../services/networking/endpoints.dart';
-import '../../../services/networking/networking_service.dart';
 
-class PromoList extends StatefulWidget {
-  const PromoList({Key? key}) : super(key: key);
-
-  @override
-  _PromoListState createState() => _PromoListState();
-}
-
-class _PromoListState extends State<PromoList> {
-  List<PromoModel> promosList = [];
-  bool isLoading = true;
-  @override
-  void initState() {
-    getPromosList();
-    super.initState();
-  }
-
-  Future<void> getPromosList() async {
-    var response =
-        await HttpService().doGet(path: Endpoints.getClientPromos(''));
-    if (response.data != null && response.data['records'].length > 0) {
-      response.data['records'].forEach((record) {
-        promosList.add(PromoModel.fromJson(record));
-      });
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
+class PromosList extends StatelessWidget {
+  const PromosList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? Column(children: const [
-            SizedBox(
-              height: PaddingSystem.padding20,
-            ),
-            CircularProgressIndicator()
-          ])
-        : promosList.isEmpty
-            ? const Center(
-                child: NoDataFound(),
-              )
-            : Container(
-                margin: const EdgeInsets.only(top: PaddingSystem.padding20),
-                child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: promosList.length,
-                    itemBuilder: (context, index) {
-                      return Promo(
-                          title: promosList[index].title,
-                          date: promosList[index].date);
-                    }));
+    List<PromoModel> list = [
+      PromoModel(
+          title: 'Recording Gear is in the tend Moving ahead in the future!',
+          imgUrl:
+              'https://static.guitarcenter.com/static/gc/2022/page-dynamic/services/lessons/guitar/special-event/mobile/gc-md-hf-guitar-lesson-specialevent-03-03-22.jpg',
+          date: '20-Mar-2022'),
+      PromoModel(
+          title: 'Get a free Guitar Lesson on buying Accoridan Guitar',
+          imgUrl:
+              'https://static.guitarcenter.com/static/gc/2021/page-dynamic/services/lessons/tablet/gc-md-bg-learning-to-play-07-19-21.jpg',
+          date: '21-Mar-2022'),
+      PromoModel(
+          title:
+              'Turn your Desktop into a fun music with our top perfoming Amplifier range starting from Rs 129.99',
+          imgUrl:
+              'https://static.guitarcenter.com/static/gc/2021/page-dynamic/services/lessons/mobile/gc-md-ad-open-house-07-19-21.jpg',
+          date: '22-Mar-2022')
+    ];
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return Promo(
+              title: list[index].title,
+              imgURL: list[index].imgUrl,
+              date: list[index].date);
+        });
   }
 }
