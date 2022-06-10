fbedd23205ba14fb2ee33afc64e822c7a7b101cb
shubhamApptware <shubham.korale@apptware.com>
2022-06-10 10:38:14 -0700

    "update_slider"

--- lib/common_widgets/slider_dynamic/slider_dynamic_call.dart
+++ lib/common_widgets/slider_dynamic/slider_dynamic_call.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:salesforce_spo/common_widgets/slider_dynamic/slider_dynamic_content.dart';
+import 'package:salesforce_spo/design_system/design_system.dart';
 import 'package:salesforce_spo/design_system/primitives/color_system.dart';
 import 'package:salesforce_spo/design_system/primitives/icon_system.dart';
 
 import 'package:salesforce_spo/design_system/primitives/landing_images.dart';
+import 'package:salesforce_spo/utils/constants.dart';
 
 class SliderDynamic extends StatefulWidget {
   SliderDynamic({Key? key, required this.name}) : super(key: key);
@@ -38,6 +40,7 @@ class _SliderDynamicState extends State<SliderDynamic> {
   Widget build(BuildContext context) {
     return Container(
       height: 170,
+      // color: Colors.red,
       margin: EdgeInsets.only(top: 20, bottom: 0),
       child: PageIndicatorContainer(
         key: key,
@@ -48,6 +51,7 @@ class _SliderDynamicState extends State<SliderDynamic> {
             _SliderWidget(name: widget.name),
             _SliderWidget(name: widget.name),
             _SliderWidget(name: widget.name)
+            // Text('4'),
           ],
           controller: controller,
           reverse: false,
@@ -72,12 +76,13 @@ class _SliderWidget extends StatelessWidget {
         Container(
           padding: EdgeInsets.symmetric(horizontal: 8.0),
           margin: EdgeInsets.only(top: 10.0, bottom: 0),
+          decoration: BoxDecoration(),
           child: Column(children: [
             Container(
               width: double.maxFinite,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20.0),
-                color: Color(0xffD6D9E0),
+                color: Color(0xffD6D9E0).withOpacity(0.5),
               ),
               padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
               child:
@@ -93,12 +98,17 @@ class _SliderWidget extends StatelessWidget {
                       children: const [
                         Text(
                           "May",
-                          style: TextStyle(fontSize: 12),
+                          style: TextStyle(
+                              fontSize: SizeSystem.size14,
+                              fontFamily: kRubik,
+                              color: Colors.white),
                         ),
                         Text(
                           '25',
                           style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w500),
+                              fontSize: SizeSystem.size20,
+                              fontFamily: kRubik,
+                              color: Colors.white),
                         )
                       ]),
                 ),
@@ -112,10 +122,11 @@ class _SliderWidget extends StatelessWidget {
                           text: TextSpan(
                               text: "call alert ",
                               style: const TextStyle(
-                                  color: Color(0xff2D3142),
-                                  fontSize: 18,
-                                  height: 1.4,
-                                  fontWeight: FontWeight.w400),
+                                color: Color(0xff2D3142),
+                                height: 1.4,
+                                fontSize: SizeSystem.size18,
+                                fontFamily: kRubik,
+                              ),
                               children: <TextSpan>[
                             TextSpan(
                                 text: name,
@@ -123,34 +134,38 @@ class _SliderWidget extends StatelessWidget {
                                     color: Color(
                                       0xff8C80F8,
                                     ),
-                                    fontSize: 18,
                                     height: 1.4,
-                                    fontWeight: FontWeight.w500))
+                                    fontSize: SizeSystem.size18,
+                                    fontFamily: kRubik,
+                                    fontWeight: FontWeight.bold))
                           ])),
                       const Text(
                         'ask for Warrenty',
                         style: TextStyle(
-                            color: Color(0xff2D3142),
-                            fontSize: 16,
-                            height: 1.4,
-                            fontWeight: FontWeight.w400),
+                          color: Color(0xff2D3142),
+                          fontSize: SizeSystem.size16,
+                          fontFamily: kRubik,
+                          height: 1.4,
+                        ),
                       ),
                       const Text(
                         'purchase',
                         style: TextStyle(
-                            color: Color(0xff2D3142),
-                            fontSize: 16,
-                            height: 1.4,
-                            fontWeight: FontWeight.w400),
+                          color: Color(0xff2D3142),
+                          fontSize: SizeSystem.size16,
+                          height: 1.4,
+                          fontFamily: kRubik,
+                        ),
                       ),
                       const Text('View Task',
                           style: TextStyle(
                               color: Color(
                                 0xff8C80F8,
                               ),
-                              fontSize: 18,
+                              fontSize: SizeSystem.size16,
                               height: 1.4,
-                              fontWeight: FontWeight.w500)),
+                              fontFamily: kRubik,
+                              fontWeight: FontWeight.bold)),
                     ],
                   ),
                 )
@@ -169,37 +184,39 @@ class _SliderWidget extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20.0),
               color: Colors.white,
+              // ignore: prefer_const_literals_to_create_immutables
               boxShadow: [
-                const BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 6.0,
-                  spreadRadius: -1,
-                  blurStyle: BlurStyle.inner,
-                  offset: Offset(
-                    0,
-                    4,
+                BoxShadow(
+                  color: Color(0xffB5AFF9).withOpacity(0.3),
+                  blurRadius: 10.0,
+                  spreadRadius: 1.0,
+                  offset: const Offset(
+                    0.0,
+                    8.0,
                   ),
                 )
               ],
             ),
+            // ignore: prefer_const_constructors
             child: Text(
-              "Today",
+              "TODAY",
               style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12),
+                  color: Color(0xff2D3142),
+                  fontSize: SizeSystem.size12,
+                  fontFamily: kRubik,
+                  fontWeight: FontWeight.w600),
             ),
           ),
         ),
         Align(
             alignment: Alignment.bottomRight,
             child: Container(
-                margin: EdgeInsets.only(right: 4, bottom: 4),
+                // color: Colors.red,
+                margin: EdgeInsets.only(right: 4, bottom: 18),
                 padding: EdgeInsets.zero,
                 child: SvgPicture.asset(
-                  LandingImages.personSlider,
-                  fit: BoxFit.cover,
-                  height: 104,
+                  LandingImages.personSlider, fit: BoxFit.cover, height: 102,
+                  // color: ColorSystem.black,
                 )))
       ],
     );
