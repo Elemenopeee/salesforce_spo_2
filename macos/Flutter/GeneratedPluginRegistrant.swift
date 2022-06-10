f27bafd0b5980547bc4607f1adaec9884dd7c912
Vivek Limbani <limbanivivek01@gmail.com>
2022-06-10 12:11:47 +0530

    created smart triggers screen UI

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import azure_ad_authentication
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AzureAdAuthenticationPlugin.register(with: registry.registrar(forPlugin: "AzureAdAuthenticationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
